e43002806af202a9dff891b090aa220fe2fd85db
ayayousef96 <112945878+ayayousef96@users.noreply.github.com>
2022-11-06 02:49:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-const regionBtn = document.getElementById('buttons');
-const countryBtn =document.getElementById('countries');
-let WorldData = {America:[],Asia :[],Africa:[], Europe:[], Oceania:[]};
-
-
 var welcome = document.querySelector('.welcome');
 document.addEventListener('DOMContentLoaded',()=>{
   setTimeout(() =>{
@@ -11,8 +6,6 @@ document.addEventListener('DOMContentLoaded',()=>{
 
 })
 
-
-//helper function for data fetxh
 async function Fetch(url){
     try {
         const result = await fetch(url);
@@ -24,14 +17,16 @@ async function Fetch(url){
     }
 };
 
-
+let WorldData = {America:[],Asia :[],Africa:[], Europe:[], Oceania:[]};
 
 
 async function fetchCountryData(){
     for(continent in WorldData){
         console.log(WorldData[continent]);
         const Region = await Fetch(`https://restcountries.com/v3.1/region/${continent}`);
+
         Region.forEach((r) =>{
+
             const countryPopulation = r.population;
             const countryName = r.name.common;
             WorldData[continent].push({country:countryName,population:countryPopulation});
@@ -40,10 +35,10 @@ async function fetchCountryData(){
         
     }
 
-
+    //console.log(WorldData);
 }
 
-const ftchCountryCities= async (country) => {
+const fetchcities = async (country) => {
     try {
         const result = await fetch('https://countriesnow.space/api/v0.1/countries/population/cities/filter', {
             method: 'POST',
@@ -71,16 +66,23 @@ const countriesBtns= document.getElementById("countries");
 
 function SetCountriesBtns(curr_region_arr){
     countriesBtns.innerHTML='';
+
     curr_region_arr.forEach((el) => {
+       
         const button = document.createElement('button');
         button.textContent = el.country;
         countriesBtns.appendChild(button);
     });
 }
+//curr_region_arr=WorldData.Asia;
+// const asia=document.getElementById('asia');
+// console.log(WorldData.Asia);
+// asia.addEventListener('click',SetCountriesBtns);
 
 
 
-
+const regionBtn = document.getElementById('buttons');
+const countryBtn =document.getElementById('countries');
 
 function handle_continent_btns(event){
     curr_region_arr=WorldData[event.target.textContent];
@@ -89,11 +91,11 @@ function handle_continent_btns(event){
     const labels = WorldData[event.target.textContent].map(el => el.country);
     const population = WorldData[event.target.textContent].map(el => el.population);
     let color=colors_by_region[event.target.textContent]
-    BulidChart(labels,population,color);
+    buildChart(labels,population,color);
 }
 
 async function handle_country_btn(event){
-    let res = await fetchDataForcountry(event.target.textContent);
+    let res = await fetchcities(event.target.textContent);
     //console.log(res);
     if (res.data) {
         const labels = res.data.map((el) => {
@@ -102,17 +104,17 @@ async function handle_country_btn(event){
         const population = res.data.map((el) => {
             return el.populationCounts[0].value
         });
-        BulidChart(labels,population,"pink");
+        buildChart(labels,population,"white");
         return;
     }
 
 }
 
-// regionBtn.addEventListener('click',handle_continent_btns);
-// countryBtn.addEventListener('click',handle_country_btn);
+regionBtn.addEventListener('click',handle_continent_btns);
+countryBtn.addEventListener('click',handle_country_btn);
 
 Chart.defaults.color='white';
-function BulidChart(labels, cur_data,color) {
+function buildChart(labels, cur_data,color) {
     const curentChart = document.getElementById('myChart');
     const myChart = new Chart(curentChart, {
         type: 'bar',
@@ -131,10 +133,3 @@ function BulidChart(labels, cur_data,color) {
         }
     });
 };
-
-function set_all_btns(){
-    regionBtn.addEventListener('click',handle_continent_btns);
-    countryBtn.addEventListener('click',handle_country_btn);
-}
-
-set_all_btns();
